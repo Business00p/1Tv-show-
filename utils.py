787fcf2acfefedc352255cb121bb99c9aeafd264
Tv show <businesskaro0565@gmail.com>
2025-01-13 20:40:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -636,10 +636,10 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                     protect_content=True if ident == "filep" else False,
                     reply_markup=InlineKeyboardMarkup(
                         [[
-                            InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-                            InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
+                            InlineKeyboardButton('⚡Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ⚡', url=GRP_LNK),
+                            InlineKeyboardButton('👻 Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ ♻️', url=CHNL_LNK)
                         ],[
-                            InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url=OWNER_LNK)
+                            InlineKeyboardButton("👨🏻‍💻 Bᴏᴛ Oᴡɴᴇʀ 👨🏻‍💻", url=OWNER_LNK)
                         ]]
                     )
                 )
