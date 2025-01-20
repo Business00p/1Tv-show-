e03301214ce3dfa330878c7b4ca4add6469940e1
Tv show <businesskaro0565@gmail.com>
2025-01-20 11:31:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/tvshowpublic')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/publicgood009')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/publicgood12')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TvshowChat') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/TvShOw076')
 
